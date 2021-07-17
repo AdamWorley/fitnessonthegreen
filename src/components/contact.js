510ac4a32e560512adf90aa32c8529868b8fde72
adamworley <worley9@hotmail.co.uk>
2021-07-17 15:06:26 +0100

    remove unused import

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-// import { useHasMounted } from "gatsby-theme-catalyst-core";
 
 export default function Contact() {
   return (
