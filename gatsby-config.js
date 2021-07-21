0943702b0d55835ea0048ee8c1a3c62994778d9b
adamworley <worley9@hotmail.co.uk>
2021-07-21 12:25:57 +0100

    remove leaflet plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,11 +52,5 @@ module.exports = {
       },
       __key: "pages",
     },
-    {
-      resolve: "gatsby-plugin-react-leaflet",
-      options: {
-        linkStyles: true, // (default: true) Enable/disable loading stylesheets via CDN
-      },
-    },
   ],
 };
