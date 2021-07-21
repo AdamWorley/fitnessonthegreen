9af40e4cc735a86de39830a1e459b8196daea67e
adamworley <worley9@hotmail.co.uk>
2021-07-21 16:09:49 +0100

    don't transpile mapbox-gl

--- src/components/map.js
+++ src/components/map.js
@@ -1,6 +1,6 @@
 // map.js
 import React, { useRef, useEffect, useState } from "react";
-import mapboxgl from "mapbox-gl";
+import mapboxgl from "!mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { useStaticQuery, graphql } from "gatsby";
 
