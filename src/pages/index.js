9ea6514d6d447e1591057b56e0178bba8dedd231
adamworley <worley9@hotmail.co.uk>
2021-10-09 17:38:48 +0100

    use contentful for home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,68 @@ import { StaticImage } from "gatsby-plugin-image";
 import * as React from "react";
 import { Contact, Layout, Navbar, Services } from "../components";
 import "../styles/main.scss";
+import { useStaticQuery, graphql } from "gatsby";
+import { BLOCKS, MARKS } from "@contentful/rich-text-types";
+import { renderRichText } from "gatsby-source-contentful/rich-text";
 
 const IndexPage = () => {
+  const homeContentQuery = useStaticQuery(graphql`
+    {
+      allContentfulLayout(filter: { slug: { eq: "home" } }) {
+        nodes {
+          contentModules {
+            ... on ContentfulCtaSection {
+              title
+              localLink
+              body {
+                raw
+              }
+            }
+            ... on ContentfulLayoutHeroImage {
+              headline
+              title
+              subTitle {
+                subTitle
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const Bold = ({ children }) => <span className="bold">{children}</span>;
+  const Text = ({ children }) => <p className="align-center">{children}</p>;
+
+  const options = {
+    renderMark: {
+      [MARKS.BOLD]: (text) => <Bold>{text}</Bold>,
+    },
+    renderNode: {
+      [BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>,
+      [BLOCKS.EMBEDDED_ASSET]: (node) => {
+        return (
+          <>
+            <h2>Embedded Asset</h2>
+            <pre>
+              <code>{JSON.stringify(node, null, 2)}</code>
+            </pre>
+          </>
+        );
+      },
+    },
+  };
+
+  const aboutSectionRichText =
+    homeContentQuery.allContentfulLayout.nodes[0].contentModules.find(
+      (x) => x.title === "About"
+    ).body;
+
+  const hero =
+    homeContentQuery.allContentfulLayout.nodes[0].contentModules.find(
+      (x) => x.title === "Home > Hero"
+    );
+
   return (
     <Layout>
       <section className="hero is-primary">
@@ -24,19 +84,10 @@ const IndexPage = () => {
             </div>
             <div className="column content is-half">
               <p className="title is-family-secondary has-text-centered is-accent">
-                YOUR FITNESS JOURNEY BROUGHT TO YOU LOCALLY
+                {hero.headline}
               </p>
-              <p className="content">
-                Are you looking for a place to train that's walking distance
-                from your home in Pease Pottage?
-                <br />
-                Are you tired of fighting for gym equipment at your local gym?
-                <br />
-                Are you looking to meet some locals whilst getting fit?
-                <br />
-                I can help you get the fitness results you are looking
-                for through 20 years of combined experience within the fitness
-                industry.
+              <p className="content" style={{ whiteSpace: "pre-line" }}>
+                {hero.subTitle.subTitle}
               </p>
             </div>
           </div>
@@ -46,7 +97,13 @@ const IndexPage = () => {
         <section id="about" className="section">
           <div className="columns is-centered">
             <div className="column is-half has-text-centered">
-              <h2 className="title ">About</h2>
+              <h2 className="title ">
+                {
+                  homeContentQuery.allContentfulLayout.nodes[0].contentModules.find(
+                    (x) => x.title === "About"
+                  ).title
+                }
+              </h2>
               {/* <figure className="image center">
                 <StaticImage
                   imgClassName="is-rounded"
@@ -57,14 +114,8 @@ const IndexPage = () => {
                 />
               </figure> */}
               <p className="is-size-5">
-                Looking for professional, certified fitness coaches that can
-                offer you a tailor-made plan that is right for you? <br />
-                Or perhaps you're looking to meet some locals whilst training in
-                the fresh outdoors, come rain or shine? <br />
-                With over half a decade of experience in fitness, Jane will help you reach your goals be it: longevity, health,
-                fat loss, muscle gain, performance, fitness, nutrition (and
-                more!) through sharing knowledge, correction of technique and
-                structured training to maximise your results.
+                {aboutSectionRichText &&
+                  renderRichText(aboutSectionRichText, options)}
               </p>
               <a className="button" href="#contact">
                 Contact
