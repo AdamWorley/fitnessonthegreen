ab1b92c9d3f03aafb1cc7a7a2bc863c178225355
adamworley <worley9@hotmail.co.uk>
2022-02-27 18:31:57 +0000

    remove/comment out warnings

--- src/components/map.js
+++ src/components/map.js
@@ -46,10 +46,10 @@ const Map = () => {
         new mapboxgl.Popup({ offset: 25 }) // add popups
           .setHTML(
             "<h3>" +
-              marker.properties.title +
-              "</h3><p>" +
-              marker.properties.description +
-              "</p>"
+            marker.properties.title +
+            "</h3><p>" +
+            marker.properties.description +
+            "</p>"
           )
       )
       .addTo(map);
